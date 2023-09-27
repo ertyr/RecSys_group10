0fcf5f3a207777beb0de803ace8df69b9c1c394c
Dimonor <72268119+Dimonor@users.noreply.github.com>
2023-09-27 17:55:47 +0200

    Added a method to compute distance

---
+++ preprocessing/Tools/Distance.py
@@ -0,0 +1,27 @@
+from math import atan2, cos, sin, sqrt
+import pandas as pd
+import pgeocode
+
+def location_user(user: pd.DataFrame) -> pd.DataFrame:
+    return pgeocode.Nominatim(user['Country']).query_postal_code(user['ZipCode'])
+
+def location_job(job: pd.DataFrame) -> pd.DataFrame:
+    return pgeocode.Nominatim(job['Country']).query_postal_code(job['Zip5'])
+    
+R = 6373.0 # radius of the earth
+def distance(location_a: pd.DataFrame, location_b: pd.DataFrame) -> float:
+    """The distance between two points
+
+    Args:
+        location_a (pd.DataFrame): output of location_user or location_job for thing a
+        location_b (pd.DataFrame): output of location_user or location_job for thing b
+
+    Returns:
+        float: approximate distance in km
+    """    
+    difference_longitude = location_b['longitude'] - location_a['longitude']
+    difference_latitude = location_b['latitude'] - location_a['latitude']
+    a = sin(difference_latitude/2)**2+ cos(location_a['latitude']) * cos(location_b['latitude']) * sin(difference_longitude / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    return R * c
+    
