ac87c6115c547833448d9051f00f03517ddfe731
Dimonor <72268119+Dimonor@users.noreply.github.com>
2023-09-27 17:20:05 +0200

    Framework for similarity done

--- preprocessing/SimilaritySuperclass.py
+++ preprocessing/SimilaritySuperclass.py
@@ -1,10 +1,11 @@
 from abc import ABC, abstractmethod
-import csv
+import pickle
+import pandas as pd
 
 
 class SimilaritySuperclass(ABC):
     @abstractmethod
-    def __similarityUserAToUserB__(self, user_A:tuple[str,...], user_B:tuple[str,...]) -> float:   
+    def __similarityUserAToUserB__(self, user_A:pd.DataFrame, user_B:pd.DataFrame) -> float:   
         """Function to implement, should provide a similarity value, 0 for no similarity and 1 for identical, direction is allowed to matter
         
         Args:
@@ -16,9 +17,8 @@ class SimilaritySuperclass(ABC):
         """        
         pass
     
-    @staticmethod
     @abstractmethod
-    def __userDirectionMatters__() -> bool:
+    def __userDirectionMatters__(self) -> bool:
         """Whether it matters if for method __similarityUserAToUserB__ the input is (A,B)  or (B,A)
         
         Returns:
@@ -26,7 +26,7 @@ class SimilaritySuperclass(ABC):
         """        
         pass
     
-    def __init__(self, file: str, values_per_user: int, index_user_ids: int, *index_to_ignore: int) -> None:
+    def __init__(self, df: pd.DataFrame, serialized_file_name: str = None) -> None:
         """Constructor for a similarity matrix class
         Args:
             file (str): the path to the file to draw the users from
@@ -35,54 +35,48 @@ class SimilaritySuperclass(ABC):
             *index_to_ignore (int): each index to ignore, may include index_user_ids
         """        
         super().__init__()
-        self.values_per_user: int = values_per_user
-        self.index_to_ignore: int = index_to_ignore
-        self.index_user_ids: tuple[int,...] = index_user_ids
-        rows = []
-        with open(file,'r') as f: reader:csv._reader = csv.reader(f, delimiter='\t')
-        first_line = False
-        for row in reader:
-            if first_line:
-                rows.append(row)
-                print(row)
-            else:
-                first_line = True
-        self.similarity_matrix: tuple[list[float]] = ([None]*len(rows),)*len(rows)
-        self.user_ids_dictionary: dict[int, int]
-        for i in range(len(rows)):
-            self.user_ids_dictionary[rows[i][index_user_ids]] = i
-            self.similarity_matrix[i][i] = 1.0
-            for j in range(i):
-                self.similarity_matrix[i][j] = self.__similarityUserAToUserB__(user_A=rows[i], user_B=rows[j])
+        self.user_to_user_similarity: dict[int, dict[int, float]] = {}
+        indexes = df.index
+        for k in range(len(indexes)):
+            i = indexes[k]
+            self.user_to_user_similarity[i] = {}
+            self.user_to_user_similarity[i][i] = 1.0
+            a = df.loc[[i]]
+            for j in indexes[:k]:
+                b = df.loc[[j]]
+                self.user_to_user_similarity[i][j] = self.__similarityUserAToUserB__(user_A=a, user_B=b)
                 if self.__userDirectionMatters__():
-                    self.similarity_matrix[j][i] = self.__similarityUserAToUserB__(user_A=rows[j], user_B=rows[i])
+                    self.user_to_user_similarity[j][i] = self.__similarityUserAToUserB__(user_A=b, user_B=a)
+                else:
+                    self.user_to_user_similarity[j][i] = self.user_to_user_similarity[i][j]
+            print(k,"/",len(indexes))
+        if serialized_file_name == None:
+            serialized_file_name = str(type(self).__name__)+'.pkl'
+        with open(serialized_file_name, 'wb') as f:
+            pickle.dump(self.user_to_user_similarity, f)
         
-                
-    
     def getSimilarityUserAToUserB(self, user_id_a: int, user_id_b: int):
-        if not self.__userDirectionMatters__():
-            a = user_id_a
-            b = user_id_b
-            user_id_a = max(a,b)
-            user_id_b = min(a,b)
-        return self.similarity_matrix[super.user_ids_dictionary[user_id_a]][super.user_ids_dictionary[user_id_b]]
-
-class SimpleSimilarity(SimilaritySuperclass):
-    def __init__(self, file: str, values_per_user: int, index_user_ids: int, *index_to_ignore: int) -> None:
-        super().__init__(file, values_per_user, index_user_ids, *index_to_ignore)
-        number_of_values_to_ignore = len(index_to_ignore)
-        if not index_user_ids in index_to_ignore:
-            number_of_values_to_ignore += 1
-        self.factor: float = values_per_user - number_of_values_to_ignore
+        return self.user_to_user_similarity[user_id_a][user_id_b]
     
-    def __similarityUserAToUserB__(self, user_A:tuple[str,...], user_B:tuple[str,...]) -> float:
-        similarity_count: int = 0
-        for i in range(super.values_per_user):
-            if not (i in super.index_to_ignore or i is super.index_user_ids) and user_A[i] == user_B[i] and not user_A[i] == "":
-                similarityCount += 1
-        return similarity_count/self.factor
-            
-    def __userDirectionMatters__() -> bool:
+class DeserializedSimilarityClass(SimilaritySuperclass):
+    def __init__(self, serialized_class_file: str, user_direction_matters) -> None:
+        self.user_direction_matters = user_direction_matters
+        with open(serialized_class_file, 'rb') as f:
+            super().user_to_user_similarity = pickle.load(f)
+    
+    def __userDirectionMatters__(self) -> bool:
+        return self.user_direction_matters
+
+class SimilaritySuperclassTest(SimilaritySuperclass):
+    def __init__(self, file: str, serialized_file_name: str = None) -> None:
+        super().__init__(file, serialized_file_name)
+    def __similarityUserAToUserB__(self, user_A:pd.DataFrame, user_B:pd.DataFrame) -> float:
+        return 1.0
+    def __userDirectionMatters__(self) -> bool:
         return False
 
-SimpleSimilarity(file="dataset\users.tsv",values_per_user=15,index_user_ids=0,index_to_ignore=1,index_to_ignore=2)
+def demo():
+    df: pd.DataFrame = pd.read_table('dataset/users.tsv', index_col='UserID')
+    max_demo_index = 100
+    df = df.iloc[range(max_demo_index)]
+    SimilaritySuperclassTest(df, "DEMO")
