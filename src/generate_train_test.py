4ad7fdd27ab10a2cc6a2a46c6a25d747aaeb5b70
ertyr <91670210+ertyr@users.noreply.github.com>
2023-09-29 15:22:12 +0200

    Add tine's work

--- src/generate_train_test.py
+++ src/generate_train_test.py
@@ -1,18 +1,44 @@
-
+import math
 import pandas
 
-full_ratings = pandas.read_csv('dataset/user_with_negative_ratings_full.csv')
 
-full_ratings_1 = full_ratings.head(10000)
-unique_users = full_ratings_1.drop_duplicates(['UserID'], keep='first').reset_index()
-final_df = pandas.DataFrame(columns=full_ratings.columns.tolist())
+NEGATIVE_NUM = 5
 
-for idx, user in unique_users.iterrows():
-    temp_df = full_ratings[full_ratings['UserID']==user['UserID']][1:]
-    final_df = pandas.concat([final_df, temp_df])
+MIN_APPLIED = 5
+MIN_TEST_SIZE = 2
+
+TEST_SIZE = 0.3
+
+ratings_full = pandas.read_csv('./dataset/user_with_negative_ratings_full.csv')
+# ratings_full = ratings_full.iloc[:100000]
+
+
+unique_user = ratings_full.drop_duplicates(['UserID']).reset_index(drop=True)
+
+ratings_train = pandas.DataFrame(columns=ratings_full.columns.tolist())
+ratings_test = pandas.DataFrame(columns=ratings_full.columns.tolist())
+
+print('Total unique users:', unique_user.shape[0])
+print('Total user ratings:', ratings_full.shape[0])
+for idx, user in unique_user.iterrows():
+    rating_users = ratings_full[ratings_full['UserID'] == user['UserID']]
+
+    if rating_users.shape[0]-NEGATIVE_NUM >= MIN_APPLIED:
+        test_num = max(MIN_TEST_SIZE, math.floor((rating_users.shape[0]-NEGATIVE_NUM)*TEST_SIZE))
+
+        temp_ratings_test = rating_users.iloc[:test_num]
+        temp_ratings_train = rating_users.iloc[test_num+1:]
+
+        ratings_test = pandas.concat([ratings_test, temp_ratings_test])
+        ratings_train = pandas.concat([ratings_train, temp_ratings_train])
 
     if idx % 5000 == 0:
-        print('Progress:', idx)
+        print('Progress at index: {:} or {:.4f} percent of total'.format(idx, idx/unique_user.shape[0]))
 
-final_df.to_csv('dataset/user_with_negative_rating_train.csv')
-unique_users.to_csv('dataset/user_with_negtive_rating_test.csv')
+
+print('User with minimum of {:} application:'.format(MIN_APPLIED), ratings_test.drop_duplicates(['UserID']).shape[0])
+print('Test size of {:} total ratings count:'.format(TEST_SIZE), ratings_test.shape[0])
+print('Train size total ratings count:', ratings_train.shape[0])
+
+ratings_test.to_csv('./dataset/user_negative_ratings_test_full.csv', index=False)
+ratings_train.to_csv('./dataset/user_negative_ratings_train_full.csv', index=False)
