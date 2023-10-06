eec407cfe77b5d30c65319522ca26ae698dea4d0
ertyr <91670210+ertyr@users.noreply.github.com>
2023-10-06 18:47:43 +0200

    More comments

--- test.py
+++ test.py
@@ -4,15 +4,17 @@ import random
 import pandas as pd
 import numpy as np
 
-
+# parameters for group generation
 size = 20
 grpNum = 20
 seed = 1234567
 
+# load user data
 user_data = pd.read_csv("dataset/users.tsv", sep='\t')
 #print(user_data.head(10))
 #print()
 
+#generate synthetic groups of users and display them
 groups2 = util.createGrps(user_data, size, grpNum)
 print(groups2)
 print()
