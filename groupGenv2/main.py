c4b46139f0711f25bae9a047b372bae52084480e
ertyr <91670210+ertyr@users.noreply.github.com>
2023-10-09 21:16:43 +0200

    Current progress

--- groupGenv2/main.py
+++ groupGenv2/main.py
@@ -1,11 +1,18 @@
 ## Import packages
 import pandas as pd
 
+## Methods
+def elimNeg(x):
+    """Eliminate neagtives"""
+    if x < 0.0:
+        return 0.0
+    return x
+
 ## Do the parameter set_up for groupGeneration
 group_size_to_create = 20
 group_number = 20
 
-## 
-
+## Get the data and elminate Tine's negative ones
 ratings_df = pd.read_csv("dataset/clean/user_ratings_neg_1000_20_20_1.csv") 
-ratings_df
+ratings_df["Rating"] = ratings_df["Rating"].apply(lambda x: elimNeg(x))
+# rename the columns in an appropriate way
