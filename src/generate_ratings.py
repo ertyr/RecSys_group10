de7ca8019af208b299bf71e676bb89cdcbcd3b8c
tinetantus <tine.tantus@gmail.com>
2023-10-09 19:11:19 +0200

    individual_explanation.ipynb completed

--- src/generate_ratings.py
+++ src/generate_ratings.py
@@ -1,9 +1,5 @@
 import pandas
 
-# TODO: More complex negative sampling technique
-NEGATIVE_SAMPLE_NUM = 10  # Number of negative sampling to add to the rating dataset
-NEGATIVE_SAMPLE_RATIO = 2.0  # Ratio determine number of negative sampling relative to positive sample in rating dataset
-
 '''
 Get the user id -> query popular_jobs -> extract JobId not exist in applied_jobs -> select top N include in rating dataset
 1. Get unique user ID
@@ -13,31 +9,35 @@ Get the user id -> query popular_jobs -> extract JobId not exist in applied_jobs
 '''
 
 
-def generate_rating_set(user_apps_df=None, popular_jobs_df=None, negative_sample_size=10):
+def generate_rating_set(user_apps_df=None, popular_jobs_df=None, negative_num=10, negative_ratio=1.0, negative_value=0):
     if user_apps_df is None:
         user_apps_df = pandas.read_table('../dataset/apps.tsv', on_bad_lines='skip')
+        print('Default user ratings:', user_apps_df.shape)
     if popular_jobs_df is None:
-        popular_jobs_df = pandas.read_csv('../dataset/popular_jobs.csv', on_bad_lines='skip')
+        popular_jobs_df = pandas.read_csv('../dataset/popular_jobs_full.csv', on_bad_lines='skip')
+        print('Default popular jobs:', popular_jobs_df.shape)
 
     unique_users = user_apps_df.drop_duplicates(['UserID'])['UserID']
     ratings_df = pandas.DataFrame(columns=['UserID', 'JobID', 'Rating'])
 
     print('Total unique user:', unique_users.shape)
-    for idx, user in zip(range(len(unique_users)),unique_users):
+    for idx, user in zip(range(len(unique_users)), unique_users):
         applied_jobs = user_apps_df[user_apps_df['UserID'] == user]['JobID'].tolist()
         temp_df = pandas.DataFrame(data=applied_jobs, columns=['JobID'])
         temp_df['UserID'] = user
         temp_df['Rating'] = 1
         ratings_df = pandas.concat([ratings_df, temp_df])
 
-        if not pandas.isna(popular_jobs_df[popular_jobs_df['UserID'] == user])['JobID'].values:
+        negative_sample_size = min(max(round(len(applied_jobs) * negative_ratio), negative_num), 50)
+
+        if not pandas.isna(popular_jobs_df[popular_jobs_df['UserID'] == user]['JobID'].iloc[0]):
 
             un_applied_jobs = popular_jobs_df[popular_jobs_df['UserID'] == user]['JobID'].iloc[0].split(' ')
             ignored_jobs = [x for x in un_applied_jobs if x not in applied_jobs][:negative_sample_size]
             temp_df = pandas.DataFrame(data=ignored_jobs, columns=['JobID'])
             temp_df['UserID'] = user
-            temp_df['Rating'] = 0
-            ratings_df = pandas.concat([ratings_df, temp_df])
+            temp_df['Rating'] = negative_value
+            ratings_df = pandas.concat([ratings_df, temp_df], ignore_index=True)
 
         if idx % 5000 == 0:
             print('Progress at index:', idx, 'size:', ratings_df.shape)
@@ -48,6 +48,7 @@ def generate_rating_set(user_apps_df=None, popular_jobs_df=None, negative_sample
 
 if __name__ == '__main__':
     user_apps = pandas.read_table('../dataset/apps.tsv', on_bad_lines='skip')
-    # user_apps_1 = user_apps[user_apps['WindowID'] == 1]
-    generated_df = generate_rating_set(user_apps_df=user_apps, negative_sample_size=5)
-    generated_df.to_csv('../dataset/user_with_negative_ratings_full.csv', index=False)
+
+    user_apps_1 = user_apps[user_apps['WindowID'] == 1]
+    generated_df = generate_rating_set(user_apps_df=user_apps_1, negative_num=5)
+    # generated_df.to_csv('../dataset/user_with_negative_ratings_full.csv', index=False)
--- src/generate_train_test.py
+++ src/generate_train_test.py
@@ -1,6 +1,6 @@
 import math
 import pandas
-
+from sklearn.model_selection import train_test_split
 
 NEGATIVE_NUM = 5
 
@@ -9,36 +9,73 @@ MIN_TEST_SIZE = 2
 
 TEST_SIZE = 0.3
 
-ratings_full = pandas.read_csv('./dataset/user_with_negative_ratings_full.csv')
-# ratings_full = ratings_full.iloc[:100000]
+
+def generate_train_test_set_legacy(ratings_full):
+    unique_user = ratings_full.drop_duplicates(['UserID']).reset_index(drop=True)
+
+    ratings_train = pandas.DataFrame(columns=ratings_full.columns.tolist())
+    ratings_test = pandas.DataFrame(columns=ratings_full.columns.tolist())
+
+    print('Total unique users:', unique_user.shape[0])
+    print('Total user ratings:', ratings_full.shape[0])
+    for idx, user in unique_user.iterrows():
+        rating_users = ratings_full[ratings_full['UserID'] == user['UserID']]
+
+        if rating_users.shape[0] - NEGATIVE_NUM >= MIN_APPLIED:
+            test_num = max(MIN_TEST_SIZE, math.floor((rating_users.shape[0] - NEGATIVE_NUM) * TEST_SIZE))
+
+            temp_ratings_test = rating_users.iloc[:test_num]
+            temp_ratings_train = rating_users.iloc[test_num + 1:]
+
+            ratings_test = pandas.concat([ratings_test, temp_ratings_test])
+            ratings_train = pandas.concat([ratings_train, temp_ratings_train])
+
+        if idx % 5000 == 0:
+            print('Progress at index: {:} or {:.4f} percent of total'.format(idx, idx / unique_user.shape[0]))
+
+    print('User with minimum of {:} application:'.format(MIN_APPLIED),
+          ratings_test.drop_duplicates(['UserID']).shape[0])
+    print('Test size of {:} total ratings count:'.format(TEST_SIZE), ratings_test.shape[0])
+    print('Train size total ratings count:', ratings_train.shape[0])
+
+    return ratings_train, ratings_test
 
 
-unique_user = ratings_full.drop_duplicates(['UserID']).reset_index(drop=True)
+def generate_train_test_set(user_ratings_full, split_ratio=0.9, split_seed=1234, min_split_size=5):
+    unique_user = user_ratings_full.drop_duplicates(['UserID']).reset_index(drop=True)
 
-ratings_train = pandas.DataFrame(columns=ratings_full.columns.tolist())
-ratings_test = pandas.DataFrame(columns=ratings_full.columns.tolist())
+    ratings_train = pandas.DataFrame(columns=user_ratings_full.columns.tolist())
+    ratings_test = pandas.DataFrame(columns=user_ratings_full.columns.tolist())
 
-print('Total unique users:', unique_user.shape[0])
-print('Total user ratings:', ratings_full.shape[0])
-for idx, user in unique_user.iterrows():
-    rating_users = ratings_full[ratings_full['UserID'] == user['UserID']]
+    print('Total unique users:', unique_user.shape[0])
+    print('Total user ratings:', user_ratings_full.shape[0])
+    for idx, user in unique_user.iterrows():
 
-    if rating_users.shape[0]-NEGATIVE_NUM >= MIN_APPLIED:
-        test_num = max(MIN_TEST_SIZE, math.floor((rating_users.shape[0]-NEGATIVE_NUM)*TEST_SIZE))
+        rating_users = user_ratings_full[user_ratings_full['UserID'] == user['UserID']]
 
-        temp_ratings_test = rating_users.iloc[:test_num]
-        temp_ratings_train = rating_users.iloc[test_num+1:]
+        if rating_users.shape[0] >= min_split_size:
+            train_split, test_split = train_test_split(rating_users, train_size=split_ratio, random_state=split_seed)
 
-        ratings_test = pandas.concat([ratings_test, temp_ratings_test])
-        ratings_train = pandas.concat([ratings_train, temp_ratings_train])
+            ratings_train = pandas.concat([ratings_train, train_split], axis=0, ignore_index=True)
+            ratings_test = pandas.concat([ratings_test, test_split], axis=0, ignore_index=True)
+        else:
+            ratings_train = pandas.concat([ratings_train, rating_users], axis=0, ignore_index=True)
 
-    if idx % 5000 == 0:
-        print('Progress at index: {:} or {:.4f} percent of total'.format(idx, idx/unique_user.shape[0]))
+        if idx % 5000 == 0:
+            print('Progress at index: {:} or {:.4f} percent of total'.format(idx, idx / unique_user.shape[0]))
+
+    print('User count:', ratings_test.drop_duplicates(['UserID']).shape[0])
+    print('Train size of {:} total ratings count:'.format(split_ratio), ratings_train.shape[0])
+    print('Test size total ratings count:', ratings_test.shape[0])
+
+    return ratings_train, ratings_test
 
 
-print('User with minimum of {:} application:'.format(MIN_APPLIED), ratings_test.drop_duplicates(['UserID']).shape[0])
-print('Test size of {:} total ratings count:'.format(TEST_SIZE), ratings_test.shape[0])
-print('Train size total ratings count:', ratings_train.shape[0])
+if __name__ == '__main__':
+    ratings_full = pandas.read_csv('./dataset/user_with_negative_ratings_full.csv')
+    # ratings_full = ratings_full.iloc[:100000]
 
-ratings_test.to_csv('./dataset/user_negative_ratings_test_full.csv', index=False)
-ratings_train.to_csv('./dataset/user_negative_ratings_train_full.csv', index=False)
+    ratings_train_df, ratings_test_df = generate_train_test_set_legacy(ratings_full)
+
+    ratings_test_df.to_csv('./dataset/user_negative_ratings_test_full.csv', index=False)
+    ratings_train_df.to_csv('./dataset/user_negative_ratings_train_full.csv', index=False)
