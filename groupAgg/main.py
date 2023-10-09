a784fb6c03aa1181bc4e9e5f81ca0604f6ba3afa
ertyr <91670210+ertyr@users.noreply.github.com>
2023-10-09 13:27:45 +0200

    For Dominic

--- groupAgg/main.py
+++ groupAgg/main.py
@@ -8,6 +8,14 @@ from lenskit.algorithms.user_knn import UserUser
 from lenskit import batch
 import sys
 
+## Methods
+def cap(x):
+    if x > 1.0:
+        return 1.0
+    elif x < 0.0:
+        return 0.0
+    return x 
+
 
 ## First get the groups
 # parameters for group generation
@@ -24,6 +32,7 @@ groups2 = createGrps(user_data, size, grpNum, seed)
 
 ## Get the training data and train the user-user collaborative filterring
 # read the data (about applications)
+# TODO: replace by Tine's filter  data
 data = pd.read_csv('dataset/user_with_negative_ratings_full.csv', delimiter=',')
 
 # construct dataframe in format (user, item, rating) via column addition
@@ -42,12 +51,16 @@ for i, row in groups2.iterrows():
     # get the array of users from the row
     synGrp = row[0]
     # create an empty dataframe to store unique jobs of this syn group
+    # TODO: maybe you don't need it. I didn't get to using it
     items_grp = pd.DataFrame(columns=["item"])
-    # iterate through each user and generate top 10 recommendations
-    db_ind = 0
     # get recommendations for all group members
     ratings_grp = batch.recommend(recsys, synGrp, n=None,  n_jobs=1)
     print(ratings_grp)
+    # cap the values (x > 1 then 1; x < 0 then 0)
+    # TODO: if you are confident that you can explain the clamping -> go ahead and use it
+    ratings_grp["score"] = ratings_grp["score"].map(cap)
+    print(ratings_grp)
+    # TODO: sys.exit only for demonstration to not run through all 20 groups of users
     sys.exit()
 
 
